ccddd7b09b52cbbfe07ab10dd9811e82f071659c
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2024-05-13 18:24:14 +0530

    added bgSyncPlugin to service worker file

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 
+const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
+    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
 workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+    new workbox.strategies.CacheFirst({
+        plugins: [bgSyncPlugin],
+    })
 );
